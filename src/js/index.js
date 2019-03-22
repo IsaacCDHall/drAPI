66cf8e7aa8f7531dfdbe3e49f395db3341cd4774
Matt Groberg <grobergmg@gmail.com>
2019-03-22 10:47:34 -0700

    add specialty

--- src/js/index.js
+++ src/js/index.js
@@ -4,11 +4,21 @@ import {DrCall} from './Drcall.js';
 
 $(document).ready(function(){
   const drSearch = new DrCall();
+  const specialties=drSearch.allSpecialties();
+  specialties.then(function(response){
+    const parsedData= JSON.parse(response);
+    console.log(parsedData);
+    parsedData.data.forEach(function(specialization){
+      $('#specialtyInput').append(`<option value="${specialization.uid}">${specialization.name}</option>`)
+    })
+
+  })
   $('#searchForm').submit(function(event){
     event.preventDefault();
     const inputedName= $('#nameInput').val();
     const condition= $('#conditionInput').val();
-    const drList= drSearch.getDrs(inputedName,condition);
+    const specialty= $('#specialtyInput').val();
+    const drList= drSearch.getDrs(inputedName,condition,specialty);
     drList.then(function(response){
       const parsedData= JSON.parse(response);
       console.log(parsedData.data);
