e3f6b0d628573c56badfa2c89b713d6e349a52e8
Matt Groberg <grobergmg@gmail.com>
2019-03-22 10:32:40 -0700

    add condition search

--- src/js/DrCall.js
+++ src/js/DrCall.js
@@ -1,14 +1,26 @@
 export class DrCall{
-  constructor(name){
-  this.name=name;
+  constructor(){
   }
 
-  getDrs(){
+  allSpecialties(){
+    return new Promise((resolve,reject)=>{
+    const myRequest = new XMLHttpRequest();
+    const url = `https://api.betterdoctor.com/2016-03-01/specialties?&user_key=${process.env.exports.apiKey}`;
+    myRequest.onload = function(){
+      if(this.status===200){
+        resolve(myRequest.response)
+      } else {
+        reject(console.log("there was an error!"))
+      }
+    }
+    myRequest.open("GET",url,true);
+    myRequest.send();
+  });
+  }
+  getDrs(name,condition){
   return new Promise((resolve,reject)=>{
   const myRequest = new XMLHttpRequest();
-  console.log("myRequest"+ myRequest.open);
-
-  const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${this.name}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+  const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&name=${name}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
   myRequest.onload = function(){
     if(this.status===200){
       resolve(myRequest.response)
@@ -20,4 +32,6 @@ export class DrCall{
   myRequest.send();
 });
   }
+
+
 }
--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,19 @@ import $ from 'jquery';
 import {DrCall} from './Drcall.js';
 
 $(document).ready(function(){
-  const drSearch = new DrCall("Jason");
-  const drList= drSearch.getDrs();
-  drList.then(function(response){
-    const parsedData= JSON.parse(response);
-    console.log(parsedData.data);
-    parsedData.data.forEach(function(dr){
-      $('.dr-list').append(`<p>${dr.profile.first_name+" "+dr.profile.last_name}</p>`)
-    })
-  })
+  const drSearch = new DrCall();
+  $('#searchForm').submit(function(event){
+    event.preventDefault();
+    const inputedName= $('#nameInput').val();
+    const condition= $('#conditionInput').val();
+    const drList= drSearch.getDrs(inputedName,condition);
+    drList.then(function(response){
+      const parsedData= JSON.parse(response);
+      console.log(parsedData.data);
+      $('.dr-list').text('');
+      parsedData.data.forEach(function(dr){
+        $('.dr-list').append(`<p>${dr.profile.first_name+" "+dr.profile.last_name}</p>`)
+      });
+    });
+  });
 });
