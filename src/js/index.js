410692a73a03e2aba9cd3dbdd72029e718be21c5
Matt Groberg <grobergmg@gmail.com>
2019-03-22 12:19:56 -0700

    pull out function for assembling dr cards

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,29 @@ import '../scss/styles.scss';
 import $ from 'jquery';
 import {DrCall} from './Drcall.js';
 
-const newPatients= function(patients){
+const newPatients=(patients)=>{
   if(patients===true){
     return "Accepting new patients";
   } else {
     return "Not accepting new patients";
   }
+}
+
+const assembleDr=(dr)=>{
+  $('.dr-list').append(`
+    <div class="drCard">
+      <h2>${dr.profile.first_name+" "+dr.profile.last_name}</h2>
+      <h4>${dr.specialties[0].name}</h4>
+      <img src='${dr.profile.image_url}'>
+      <div class="practice">
+        <p>${newPatients(dr.practices[0].accepts_new_patients)}</p>
+        <p>${dr.practices[0].phones[0].number}</p>
+        <p>${dr.practices[0].email}</p>
+        <p>${dr.practices[0].website}</p>
+      </div>
+      <p class="bio">${dr.profile.bio}<p>
+    </div>
+    `);
 }
 
 $(document).ready(function(){
@@ -18,9 +35,8 @@ $(document).ready(function(){
     console.log(parsedData);
     parsedData.data.forEach(function(specialization){
       $('#specialtyInput').append(`<option value="${specialization.uid}">${specialization.name}</option>`)
-    })
-
-  })
+    });
+  });
   $('#searchForm').submit(function(event){
     event.preventDefault();
     const inputedName= $('#nameInput').val();
@@ -32,20 +48,7 @@ $(document).ready(function(){
       console.log(parsedData.data);
       $('.dr-list').text('');
       parsedData.data.forEach(function(dr){
-        $('.dr-list').append(`
-          <div class="drCard">
-            <h2>${dr.profile.first_name+" "+dr.profile.last_name}</h2>
-            <h4>${dr.specialties[0].name}</h4>
-            <img src='${dr.profile.image_url}'>
-            <div class="practice">
-              <p>${newPatients(dr.practices[0].accepts_new_patients)}</p>
-              <p>${dr.practices[0].phones[0].number}</p>
-              <p>${dr.practices[0].email}</p>
-              <p>${dr.practices[0].website}</p>
-            </div>
-            <p class="bio">${dr.profile.bio}<p>
-          </div>
-          `)
+        assembleDr(dr);
       });
     });
   });
