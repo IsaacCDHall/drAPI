208fc8e9c5f70c5f6c62ef6300826aa410553449
Matt Groberg <grobergmg@gmail.com>
2019-03-22 09:07:13 -0700

    add basic api call

---
+++ src/js/DrCall.js
@@ -0,0 +1,23 @@
+export class DrCall{
+  constructor(){
+
+  }
+
+  getDrs(){
+  return new Promise((resolve,reject)=>{
+  const myRequest = new XMLHttpRequest();
+  console.log("myRequest"+ myRequest.open);
+
+  const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+  myRequest.onload = function(){
+    if(this.status===200){
+      resolve(myRequest.response)
+    } else {
+      reject(console.log("there was an error!"))
+    }
+  }
+  myRequest.open("GET",url,true);
+  myRequest.send();
+});
+  }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
 import '../scss/styles.scss';
 import $ from 'jquery';
+import {DrCall} from './Drcall.js';
 
 $(document).ready(function(){
-  alert("jquery connected");
+  const drSearch = new DrCall();
+  const drList= drSearch.getDrs();
+  console.log(drList);
 });
