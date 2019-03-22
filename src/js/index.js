ab63b722630922684844ba047d42a676f5628799
Matt Groberg <grobergmg@gmail.com>
2019-03-22 09:21:46 -0700

    append names to html

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,9 @@ $(document).ready(function(){
   const drList= drSearch.getDrs();
   drList.then(function(response){
     const parsedData= JSON.parse(response);
-    console.log(parsedData);
+    console.log(parsedData.data);
+    parsedData.data.forEach(function(dr){
+      $('.dr-list').append(`<p>${dr.profile.first_name+" "+dr.profile.last_name}</p>`)
+    })
   })
 });
