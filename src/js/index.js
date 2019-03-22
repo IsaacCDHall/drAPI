98335ac15dc6df6b44f450a594118899fbefdda7
Matt Groberg <grobergmg@gmail.com>
2019-03-22 11:25:19 -0700

    add additional info to each dr card

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,14 @@ import '../scss/styles.scss';
 import $ from 'jquery';
 import {DrCall} from './Drcall.js';
 
+const newPatients= function(patients){
+  if(patients===true){
+    return "Accepting new patients";
+  } else {
+    return "Not accepting new patients";
+  }
+}
+
 $(document).ready(function(){
   const drSearch = new DrCall();
   const specialties=drSearch.allSpecialties();
@@ -24,7 +32,19 @@ $(document).ready(function(){
       console.log(parsedData.data);
       $('.dr-list').text('');
       parsedData.data.forEach(function(dr){
-        $('.dr-list').append(`<p>${dr.profile.first_name+" "+dr.profile.last_name}</p>`)
+        $('.dr-list').append(`
+          <div class="drCard">
+            <h2>${dr.profile.first_name+" "+dr.profile.last_name}</h2>
+            <img src='${dr.profile.image_url}'>
+            <div class="practice">
+              <p>${newPatients(dr.practices[0].accepts_new_patients)}</p>
+              <p>${dr.practices[0].phones[0].number}</p>
+              <p>${dr.practices[0].email}</p>
+              <p>${dr.practices[0].website}</p>
+            </div>
+            <p class="bio">${dr.profile.bio}<p>
+          </div>
+          `)
       });
     });
   });
